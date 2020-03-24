7aca42fbd706f1ded3db474dd8517749b0086706
Rijad Dzanko <rijadda@gmail.com>
2020-03-24 15:11:26 +0100

    MockUp

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('stickyMenu', null) menuElement: ElementRef;
+
+  // tslint:disable-next-line: no-inferrable-types
+  sticky: boolean = false;
+  elementPosition: any;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngAfterViewInit() {
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  }
+  @HostListener('window:scroll', ['$event'])
+    handleScroll() {
+      const windowScroll = window.pageYOffset;
+      if (windowScroll >= this.elementPosition) {
+        this.sticky = true;
+      } else {
+        this.sticky = false;
+      }
+    }
 }
